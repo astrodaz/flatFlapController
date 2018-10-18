7d9228d5a7523c2a056b63466a3ed71ff2c1f645
Daz <Daz@ELITEBOOK>
2018-10-18 19:14:00 +0100

    Initial upload

---
+++ flatFlapController/frmMain.Designer.vb
@@ -0,0 +1,270 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class formMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.buttonExit = New System.Windows.Forms.Button()
+        Me.groupSerialFeedback = New System.Windows.Forms.GroupBox()
+        Me.textSerialData = New System.Windows.Forms.TextBox()
+        Me.buttonFlap = New System.Windows.Forms.Button()
+        Me.buttonEL = New System.Windows.Forms.Button()
+        Me.buttonNudgeCW = New System.Windows.Forms.Button()
+        Me.buttonNudgeCCW = New System.Windows.Forms.Button()
+        Me.txtNudgeValue = New System.Windows.Forms.TextBox()
+        Me.buttonSetZero = New System.Windows.Forms.Button()
+        Me.buttonSetOpenPosition = New System.Windows.Forms.Button()
+        Me.textOpenPosition = New System.Windows.Forms.TextBox()
+        Me.buttonLED = New System.Windows.Forms.Button()
+        Me.comboLED = New System.Windows.Forms.ComboBox()
+        Me.buttonFeedback = New System.Windows.Forms.Button()
+        Me.comboPort = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.buttonStats = New System.Windows.Forms.Button()
+        Me.buttonReset = New System.Windows.Forms.Button()
+        Me.buttonConnect = New System.Windows.Forms.Button()
+        Me.groupSerialFeedback.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'buttonExit
+        '
+        Me.buttonExit.Location = New System.Drawing.Point(524, 359)
+        Me.buttonExit.Name = "buttonExit"
+        Me.buttonExit.Size = New System.Drawing.Size(72, 23)
+        Me.buttonExit.TabIndex = 1
+        Me.buttonExit.Text = "Exit"
+        Me.buttonExit.UseVisualStyleBackColor = True
+        '
+        'groupSerialFeedback
+        '
+        Me.groupSerialFeedback.Controls.Add(Me.buttonConnect)
+        Me.groupSerialFeedback.Controls.Add(Me.buttonReset)
+        Me.groupSerialFeedback.Controls.Add(Me.buttonStats)
+        Me.groupSerialFeedback.Controls.Add(Me.Label1)
+        Me.groupSerialFeedback.Controls.Add(Me.comboPort)
+        Me.groupSerialFeedback.Controls.Add(Me.buttonFeedback)
+        Me.groupSerialFeedback.Controls.Add(Me.textSerialData)
+        Me.groupSerialFeedback.Location = New System.Drawing.Point(20, 21)
+        Me.groupSerialFeedback.Name = "groupSerialFeedback"
+        Me.groupSerialFeedback.Size = New System.Drawing.Size(591, 256)
+        Me.groupSerialFeedback.TabIndex = 2
+        Me.groupSerialFeedback.TabStop = False
+        Me.groupSerialFeedback.Text = "Serial Feedback"
+        '
+        'textSerialData
+        '
+        Me.textSerialData.Enabled = False
+        Me.textSerialData.Location = New System.Drawing.Point(12, 22)
+        Me.textSerialData.Multiline = True
+        Me.textSerialData.Name = "textSerialData"
+        Me.textSerialData.Size = New System.Drawing.Size(472, 217)
+        Me.textSerialData.TabIndex = 0
+        '
+        'buttonFlap
+        '
+        Me.buttonFlap.BackColor = System.Drawing.SystemColors.Control
+        Me.buttonFlap.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.buttonFlap.Location = New System.Drawing.Point(20, 298)
+        Me.buttonFlap.Name = "buttonFlap"
+        Me.buttonFlap.Size = New System.Drawing.Size(75, 23)
+        Me.buttonFlap.TabIndex = 3
+        Me.buttonFlap.Text = "Flap"
+        Me.buttonFlap.UseVisualStyleBackColor = False
+        '
+        'buttonEL
+        '
+        Me.buttonEL.Location = New System.Drawing.Point(20, 327)
+        Me.buttonEL.Name = "buttonEL"
+        Me.buttonEL.Size = New System.Drawing.Size(75, 23)
+        Me.buttonEL.TabIndex = 4
+        Me.buttonEL.Text = "EL Panel"
+        Me.buttonEL.UseVisualStyleBackColor = True
+        '
+        'buttonNudgeCW
+        '
+        Me.buttonNudgeCW.Location = New System.Drawing.Point(139, 298)
+        Me.buttonNudgeCW.Name = "buttonNudgeCW"
+        Me.buttonNudgeCW.Size = New System.Drawing.Size(75, 23)
+        Me.buttonNudgeCW.TabIndex = 7
+        Me.buttonNudgeCW.Text = "Nudge CW"
+        Me.buttonNudgeCW.UseVisualStyleBackColor = True
+        '
+        'buttonNudgeCCW
+        '
+        Me.buttonNudgeCCW.Location = New System.Drawing.Point(139, 356)
+        Me.buttonNudgeCCW.Name = "buttonNudgeCCW"
+        Me.buttonNudgeCCW.Size = New System.Drawing.Size(75, 23)
+        Me.buttonNudgeCCW.TabIndex = 8
+        Me.buttonNudgeCCW.Text = "Nudge CCW"
+        Me.buttonNudgeCCW.UseVisualStyleBackColor = True
+        '
+        'txtNudgeValue
+        '
+        Me.txtNudgeValue.Location = New System.Drawing.Point(151, 330)
+        Me.txtNudgeValue.MaxLength = 2
+        Me.txtNudgeValue.Name = "txtNudgeValue"
+        Me.txtNudgeValue.Size = New System.Drawing.Size(53, 20)
+        Me.txtNudgeValue.TabIndex = 9
+        '
+        'buttonSetZero
+        '
+        Me.buttonSetZero.Location = New System.Drawing.Point(260, 298)
+        Me.buttonSetZero.Name = "buttonSetZero"
+        Me.buttonSetZero.Size = New System.Drawing.Size(75, 23)
+        Me.buttonSetZero.TabIndex = 10
+        Me.buttonSetZero.Text = "Set Zero"
+        Me.buttonSetZero.UseVisualStyleBackColor = True
+        '
+        'buttonSetOpenPosition
+        '
+        Me.buttonSetOpenPosition.Location = New System.Drawing.Point(260, 330)
+        Me.buttonSetOpenPosition.Name = "buttonSetOpenPosition"
+        Me.buttonSetOpenPosition.Size = New System.Drawing.Size(75, 23)
+        Me.buttonSetOpenPosition.TabIndex = 11
+        Me.buttonSetOpenPosition.Text = "Set Open"
+        Me.buttonSetOpenPosition.UseVisualStyleBackColor = True
+        '
+        'textOpenPosition
+        '
+        Me.textOpenPosition.Location = New System.Drawing.Point(268, 359)
+        Me.textOpenPosition.MaxLength = 4
+        Me.textOpenPosition.Name = "textOpenPosition"
+        Me.textOpenPosition.Size = New System.Drawing.Size(55, 20)
+        Me.textOpenPosition.TabIndex = 12
+        '
+        'buttonLED
+        '
+        Me.buttonLED.Location = New System.Drawing.Point(376, 298)
+        Me.buttonLED.Name = "buttonLED"
+        Me.buttonLED.Size = New System.Drawing.Size(75, 23)
+        Me.buttonLED.TabIndex = 14
+        Me.buttonLED.Text = "Set LED"
+        Me.buttonLED.UseVisualStyleBackColor = True
+        '
+        'comboLED
+        '
+        Me.comboLED.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.comboLED.FormattingEnabled = True
+        Me.comboLED.Location = New System.Drawing.Point(379, 332)
+        Me.comboLED.Name = "comboLED"
+        Me.comboLED.Size = New System.Drawing.Size(72, 21)
+        Me.comboLED.TabIndex = 15
+        '
+        'buttonFeedback
+        '
+        Me.buttonFeedback.Location = New System.Drawing.Point(501, 22)
+        Me.buttonFeedback.Name = "buttonFeedback"
+        Me.buttonFeedback.Size = New System.Drawing.Size(75, 53)
+        Me.buttonFeedback.TabIndex = 14
+        Me.buttonFeedback.Text = "Toggle Feedback"
+        Me.buttonFeedback.UseVisualStyleBackColor = True
+        '
+        'comboPort
+        '
+        Me.comboPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.comboPort.FormattingEnabled = True
+        Me.comboPort.Location = New System.Drawing.Point(501, 120)
+        Me.comboPort.Name = "comboPort"
+        Me.comboPort.Size = New System.Drawing.Size(75, 21)
+        Me.comboPort.TabIndex = 15
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(498, 104)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 16
+        Me.Label1.Text = "Serial Port"
+        '
+        'buttonStats
+        '
+        Me.buttonStats.Location = New System.Drawing.Point(501, 178)
+        Me.buttonStats.Name = "buttonStats"
+        Me.buttonStats.Size = New System.Drawing.Size(75, 23)
+        Me.buttonStats.TabIndex = 17
+        Me.buttonStats.Text = "Values"
+        Me.buttonStats.UseVisualStyleBackColor = True
+        '
+        'buttonReset
+        '
+        Me.buttonReset.Location = New System.Drawing.Point(501, 216)
+        Me.buttonReset.Name = "buttonReset"
+        Me.buttonReset.Size = New System.Drawing.Size(75, 23)
+        Me.buttonReset.TabIndex = 18
+        Me.buttonReset.Text = "Reset"
+        Me.buttonReset.UseVisualStyleBackColor = True
+        '
+        'buttonConnect
+        '
+        Me.buttonConnect.Location = New System.Drawing.Point(501, 147)
+        Me.buttonConnect.Name = "buttonConnect"
+        Me.buttonConnect.Size = New System.Drawing.Size(75, 23)
+        Me.buttonConnect.TabIndex = 19
+        Me.buttonConnect.Text = "Connect"
+        Me.buttonConnect.UseVisualStyleBackColor = True
+        '
+        'formMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(631, 399)
+        Me.Controls.Add(Me.comboLED)
+        Me.Controls.Add(Me.buttonLED)
+        Me.Controls.Add(Me.textOpenPosition)
+        Me.Controls.Add(Me.buttonSetOpenPosition)
+        Me.Controls.Add(Me.buttonSetZero)
+        Me.Controls.Add(Me.txtNudgeValue)
+        Me.Controls.Add(Me.buttonNudgeCCW)
+        Me.Controls.Add(Me.buttonNudgeCW)
+        Me.Controls.Add(Me.buttonEL)
+        Me.Controls.Add(Me.buttonFlap)
+        Me.Controls.Add(Me.groupSerialFeedback)
+        Me.Controls.Add(Me.buttonExit)
+        Me.Name = "formMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "FlatFlap Stepper Controller"
+        Me.groupSerialFeedback.ResumeLayout(False)
+        Me.groupSerialFeedback.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents buttonExit As Button
+    Friend WithEvents groupSerialFeedback As GroupBox
+    Friend WithEvents textSerialData As TextBox
+    Friend WithEvents buttonFlap As Button
+    Friend WithEvents buttonEL As Button
+    Friend WithEvents buttonNudgeCW As Button
+    Friend WithEvents buttonNudgeCCW As Button
+    Friend WithEvents txtNudgeValue As TextBox
+    Friend WithEvents buttonSetZero As Button
+    Friend WithEvents buttonSetOpenPosition As Button
+    Friend WithEvents textOpenPosition As TextBox
+    Friend WithEvents buttonLED As Button
+    Friend WithEvents comboLED As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents comboPort As ComboBox
+    Friend WithEvents buttonFeedback As Button
+    Friend WithEvents buttonStats As Button
+    Friend WithEvents buttonReset As Button
+    Friend WithEvents buttonConnect As Button
+End Class
---
+++ flatFlapController/frmMain.vb
@@ -0,0 +1,123 @@
+﻿Public Class formMain
+
+    Dim serialPort As New IO.Ports.SerialPort
+
+
+    Private Sub buttonExit_Click(sender As Object, e As EventArgs) Handles buttonExit.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub formMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' Load the allowable LED values into the combo box
+        comboLED.Items.Add("13")
+        comboLED.Items.Add("12")
+        comboLED.Items.Add("7")
+        comboLED.Items.Add("6")
+        comboLED.Items.Add("5")
+        comboLED.Items.Add("4")
+        comboLED.Items.Add("3")
+        comboLED.Items.Add("2")
+
+        ' Set the default values
+        ' LED Pin
+        comboLED.SelectedIndex = comboLED.FindStringExact("13")
+
+        ' Nudge value
+        txtNudgeValue.Text = "25"
+
+        ' Open position
+        textOpenPosition.Text = "1356"
+
+        ' Serial Ports
+        GetComPorts()
+
+    End Sub
+
+
+    Private Sub GetComPorts()
+
+        ' Declare Ports 
+        Dim Ports As String() = IO.Ports.SerialPort.GetPortNames()
+        ' Add port name Into a comboBox control 
+        For Each Port In Ports
+            comboPort.Items.Add(Port)
+        Next Port
+        ' Select an item in the combobox 
+        If comboPort.Items.Count > 0 Then
+            comboPort.SelectedIndex = 0
+        End If
+
+    End Sub
+
+    Private Sub comboPort_SelectedIndexChanged(sender As Object, e As EventArgs) Handles comboPort.SelectedIndexChanged
+
+        serialPort.PortName = comboPort.SelectedItem
+        serialPort.BaudRate = 9600
+        serialPort.ReadTimeout = 10000
+
+    End Sub
+
+    Private Sub buttonConnect_Click(sender As Object, e As EventArgs) Handles buttonConnect.Click
+
+        serialPort.Open()
+        serialPort.Write(":HH#")
+
+    End Sub
+
+    Private Sub buttonFlap_Click(sender As Object, e As EventArgs) Handles buttonFlap.Click
+
+        Dim serialData As String
+        serialPort.Write(":TF#")
+        serialData = serialPort.ReadLine()
+        textSerialData.Text += serialData
+        parseSerialData(serialData)
+
+    End Sub
+
+
+    Private Sub readSerialData()
+
+        Dim returnString As String = ""
+        Dim receivedChars As String = ""
+
+        Try
+            Do
+                receivedChars = serialPort.ReadLine()
+                If receivedChars Is Nothing Then
+                    Exit Do
+                Else
+                    returnString = receivedChars + vbCrLf
+                    receivedChars = ""
+                    receivedChars = returnString.TrimStart(":")
+                    returnString = receivedChars
+                    receivedChars = returnString.TrimEnd("#")
+                    parseSerialData(receivedChars)
+                End If
+
+            Loop
+
+        Catch ex As TimeoutException
+            returnString = "Error: Serial Port read timed out."
+
+        End Try
+
+    End Sub
+
+    Private Sub parseSerialData(serialString As String)
+
+        ' Process feedback from the serial port
+        Select Case (serialString)
+            Case "FO"
+                textSerialData.Text += "Flap toggled - now OPEN"
+                buttonFlap.BackColor = Color.Lime
+            Case "FC"
+                textSerialData.Text += "Flap toggled - now CLOSED"
+                buttonFlap.BackColor = Color.Red
+        End Select
+    End Sub
+End Class
